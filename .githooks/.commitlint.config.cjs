d4e35510f8da58e5e29f9dcb5ef2ecd5e6edae29
Nilesh Kesar <NHKesar@gmail.com>
2026-05-28 14:02:52 -0400

    Update .commitlint.config.cjs

--- .githooks/.commitlint.config.cjs
+++ .githooks/.commitlint.config.cjs
@@ -2,5 +2,10 @@ module.exports = {
   extends: ["@commitlint/config-conventional"],
   rules: {
     "subject-case": [0],
+    // Disable body length checks
+    "body-max-line-length": [0],
+    "body-max-length": [0],
+    "footer-max-line-length": [0],
+    "footer-max-length": [0]
   },
 };
